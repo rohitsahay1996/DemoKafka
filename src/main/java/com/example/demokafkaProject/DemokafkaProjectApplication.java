9ce962567f48c3a567a237c3db47c67546737340
rohitsahay <rohitsahay.199626@gmail.com>
2020-01-13 20:13:34 +0530

    first

---
+++ src/main/java/com/example/demokafkaProject/DemokafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demokafkaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokafkaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemokafkaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demokafkaProject/DemokafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demokafkaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemokafkaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
